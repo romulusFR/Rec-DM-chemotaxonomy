0c6a870c49d37a804b6b5ba5f1e4ab3a805127b0
Romuald THION <romuald.thion@univ-nc.nc>
2021-09-11 09:10:20 +1100

    doc

--- scopus/scopus.py
+++ scopus/scopus.py
@@ -18,7 +18,7 @@ import pandas as pd
 
 logging.basicConfig()
 logger = logging.getLogger("scopus_api")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 # https://www.twilio.com/blog/asynchronous-http-requests-in-python-with-aiohttp
 # https://stackoverflow.com/questions/48682147/aiohttp-rate-limiting-parallel-requests
@@ -44,37 +44,38 @@ OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 CSV_PARAMS = {"delimiter": ";", "quotechar": '"'}
 
 # API Scopus
-MAX_REQ_BY_SEC = 50
+MAX_REQ_BY_SEC = 500
 API_KEY = {"X-ELS-APIKey": "7047b3a8cf46d922d5d5ca71ff531b7d"}
 X_RATE_HEADERS = ["X-RateLimit-Limit", "X-RateLimit-Remaining", "X-RateLimit-Reset"]
 
 
+async def query_fake(keyword1, keyword2, /, delay=1):
+    """Fake query tool without network, for test purpose"""
+    results_nb = randint(1, 10000)
+    await asyncio.sleep(delay)
+    start_time = datetime.datetime.now()
+    logger.debug("query_fake(%s, %s): launching at %s", keyword1, keyword2, start_time)
+    await asyncio.sleep(randint(1, 1000) / 1000)
+    elapsed = datetime.datetime.now() - start_time
+    return (keyword1, keyword2, results_nb, elapsed.seconds + elapsed.microseconds / 10 ** 6)
+
+
 def build_search_query(keywords1, keywords2):
+    """transform two keywords into a SCOPUS API query string. Splits keywords as OR subqueries if needed"""
     return {
         "query": f'( KEY ( {keywords1} ) AND KEY ( {keywords2} ) ) AND ( DOCTYPE( "ar" ) )',
         "count": 1,
     }
 
 
-async def query_mockup(keyword1, keyword2, /, delay=1):
-    """launcher demo"""
-    results_nb = randint(1, 10000)
-    await asyncio.sleep(delay)
-    start_time = datetime.datetime.now()
-    logger.debug("run_async_query_mockup(%s, %s): launching at %s", keyword1, keyword2, start_time)
-    await asyncio.sleep(randint(1, 1000) / 1000)
-    elapsed = datetime.datetime.now() - start_time
-    return (keyword1, keyword2, results_nb, elapsed.seconds + elapsed.microseconds / 10 ** 6)
-
-
 async def query_httpbin(keyword1, keyword2, /, delay=1):
-    """launcher demo"""
+    """Fake query tool WITH network on httpbin, for test purpose"""
     url = "http://httpbin.org/anything"
     data = {"answer": randint(1, 10000)}
     results_nb = -1
     await asyncio.sleep(delay)
     start_time = datetime.datetime.now()
-    logger.debug("run_async_query_test(%s, %s): launching at %s", keyword1, keyword2, start_time)
+    logger.debug("query_httpbin(%s, %s): launching at %s", keyword1, keyword2, start_time)
     async with aiohttp.ClientSession() as session:
         try:
             async with session.get(url, params=build_search_query(keyword1, keyword2), data=data) as resp:
@@ -92,12 +93,12 @@ async def query_httpbin(keyword1, keyword2, /, delay=1):
 
 
 async def query_scopus(keyword1, keyword2, /, delay=1):
-    """launcher"""
+    """SCOPUS query tool: return the number of article papers having two sets of keywords. Delay is in sec"""
     scopus_url = "https://api.elsevier.com/content/search/scopus"
     results_nb = -1
     await asyncio.sleep(delay)
     start_time = datetime.datetime.now()
-    logger.debug("run_async_query(%s, %s) @%s", keyword1, keyword2, start_time)
+    logger.debug("query_scopus(%s, %s) @%s", keyword1, keyword2, start_time)
     async with aiohttp.ClientSession() as session:
         try:
             async with session.get(scopus_url, params=build_search_query(keyword1, keyword2), headers=API_KEY) as resp:
@@ -115,68 +116,85 @@ async def query_scopus(keyword1, keyword2, /, delay=1):
         return (keyword1, keyword2, results_nb, elapsed.seconds + elapsed.microseconds / 10 ** 6)
 
 
-MODES = {"scopus": query_scopus, "httpbin": query_httpbin, "mock": query_mockup}
+# query modes : one fake, one fake over network, one true
+MODES = {"scopus": query_scopus, "httpbin": query_httpbin, "fake": query_fake}
+BASE_RESPONSE_TIME = 1.0
 
 
 async def main(pairs, mode):
-    """Create tasks sequentially with throttling"""
+    """Create tasks sequentially with throttling : one query every 1/MAX_REQ_BY_SEC second"""
     coros = []
-    task_factory = MODES.get(mode, MODES["mock"])
+    # pick the query mode
+    task_factory = MODES.get(mode, MODES["fake"])
+    # build all the tasks, each one sending an http network request over the network after some delay
+    # delay is incremented by 1/MAX_REQ_BY_SEC
     for i, (chemo, pharma) in enumerate(pairs):
         delay = i * 1 / MAX_REQ_BY_SEC
         task = asyncio.create_task(task_factory(chemo, pharma, delay=delay))
         coros.append(task)
-    logger.info("main: %i jobs created", len(pairs))
+    logger.info(
+        "main: %i jobs created, estimated duration %f sec", len(pairs), len(pairs) / MAX_REQ_BY_SEC + BASE_RESPONSE_TIME
+    )
 
     res = await asyncio.gather(*coros)
     res_dict = defaultdict(dict)
     for (chemo, pharma, nb_results, _) in res:
         res_dict[chemo][pharma] = nb_results
     return res_dict
-    # return {(chemo, pharma): (nb, duration) for (chemo, pharma, nb, duration) in res}
 
 
-def clean_word(string: str) -> str:
-    """standardize"""
+def standardize(string: str) -> str:
+    """Standardize strings"""
     return string.strip().lower()
 
 
 def get_classes(filename):
-    """Loads classes from a csv file with format (parent, child)"""
+    """Loads classes from a csv file with format (parent, child) and return a dict with ALL names"""
     logger.debug("get_classes(%s)", filename)
     classes = {}
     with open(filename, encoding="utf-8") as csvfile:
         reader = csv.reader(csvfile, **CSV_PARAMS)
         for (parent, child) in reader:
-            classes[clean_word(child)] = clean_word(parent)
+            classes[standardize(child)] = standardize(parent)
 
+    # add parents classes to the dict, with a default parent for each one
     for value in set(classes.values()):
         classes[value] = BASE_CLASS
     return classes
 
 
 # NOTE : rows et cols sont surtout pour l'ordreet les cases vides si besoin
-def write_results(res_dict: dict[Tuple[str, str], Tuple[int, float]], rows, cols, filename):
+def write_results(res_dict: dict[Tuple[str, str], Tuple[int, float]], rows, cols, row_classes, col_classes, filename):
     """Store result dict"""
     logger.debug("write_results(%i, %s)", len(results), filename)
     with open(filename, "w", newline="", encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile, **CSV_PARAMS)
-        writer.writerow(("/", *cols))
+        # pharmacological activity classes
+        writer.writerow(("", "", *(col_classes[col] for col in cols)))
+        # pharmacological activities
+        writer.writerow(("", "", *cols))
+        # write compounds, one by line
         for row in rows:
             values = [res_dict[row].get(col, -1) for col in cols]  # type:ignore
-            writer.writerow((row, *values))
-    logger.debug("%s written", filename)
+            writer.writerow((row_classes[row], row, *values))
+
+
+def sorted_keys(classes, base_only=True):
+    """Ensure that parents classes are first in order, and that children are classes-wise ordered alphabeticaly"""
+    return sorted(
+        (com for com, cls in classes.items() if not base_only or cls == BASE_CLASS), key=lambda x: (classes[x], x)
+    )
 
 
 # %%
-
+BASE_ONLY=True
 if __name__ == "__main__":
     compounds = get_classes(COMPOUNDS)
     pharmaco = get_classes(PHARMACOLOGY)
-    # TODO ici, trier différement : classes puis values
-    compounds_keywords = {v for v in compounds.values() if v != BASE_CLASS}
-    pharmaco_keywords = {v for v in pharmaco.values() if v != BASE_CLASS}
+    compounds_keywords = sorted_keys(compounds, base_only=BASE_ONLY)
+    pharmaco_keywords = sorted_keys(pharmaco, base_only=BASE_ONLY)
     queries = list(product(compounds_keywords, pharmaco_keywords))
+    logger.info("%i compounds X %i pharmacology = %i", len(compounds_keywords), len(pharmaco_keywords), len(queries))
 
     main_start_time = datetime.datetime.now()
     logger.info("START")
@@ -184,9 +202,10 @@ if __name__ == "__main__":
     total_time = datetime.datetime.now() - main_start_time
     logger.info("DONE in %fs", total_time.seconds + total_time.microseconds / 10 ** 6)
 
-    output_filename = OUTPUT_DIR / f"activity_{datetime.datetime.now()}.csv"
-    write_results(results, compounds_keywords, pharmaco_keywords, output_filename)
-    logger.info("WRITTEN in %s", output_filename)
+    # output_filename = OUTPUT_DIR / f"activity_{datetime.datetime.now()}.csv"
+    output_filename = OUTPUT_DIR / f"activity_{'test'}.csv"
+    write_results(results, compounds_keywords, pharmaco_keywords, compounds, pharmaco, output_filename)
+    logger.info("WRITTEN %s", output_filename)
 
 
 # if __name__ == "__main__":
