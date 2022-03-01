717979110691d227cb80b6b9ac0ac3cff71e197d
Romuald THION <romuald.thion@univ-nc.nc>
2022-03-01 17:16:29 +1100

    calculs ods + w/ w/o inversé

--- scopus/transform_mrcv.py
+++ scopus/transform_mrcv.py
@@ -1,14 +1,17 @@
+"""Using Bilder's MRCV waste dataset"""
 # %%
 
 
+from itertools import product
+
+import numpy as np
 import pandas as pd
-from itertools import product, islice
 
 BOOLS = [False, True]
 SELECTORS = ["w/o", "w/"]  # ordered as bools
 
 # read raw data file written as 3 + 4 = 7 bits
-df = pd.read_csv("data/2_mrcv.txt", sep=" ")
+raw = pd.read_csv("data/2_mrcv.txt", sep=" ")
 
 # constants : rows and columns
 wastes = [
@@ -17,21 +20,22 @@ wastes = [
     "Salt",
 ]
 storages = ["Lagoon", "Pit", "Drainage", "Tank"]
-df.columns = wastes + storages
+raw.columns = wastes + storages
+
+I, J = len(wastes), len(storages)
+print(I, J)
 
 # grand total
-N = len(df)
+N = len(raw)
 
-assert df["Tank"].sum() == 13
+assert raw["Tank"].sum() == 13
 
 # %%
 # df for results
-res = pd.DataFrame(
-    index=pd.MultiIndex.from_tuples(list(product(wastes, SELECTORS))),
-    columns=pd.MultiIndex.from_tuples(list(product(storages, SELECTORS))),
+df = pd.DataFrame(
+    index=pd.MultiIndex.from_tuples(list(product(wastes, reversed(SELECTORS)))),
+    columns=pd.MultiIndex.from_tuples(list(product(storages, reversed(SELECTORS)))),
 ).fillna(0)
-
-res
 
 
 # res.loc["Salt", "Pit"]
@@ -40,9 +44,9 @@ res
 
 # %%
 # for each combination (waste, storage), add 1 to corresponding celle of the subtable
-res.loc[:] = 0
-for c in df.itertuples():
-# for c in islice(df.itertuples(),10):
+df.loc[:] = 0
+for c in raw.itertuples():
+    # for c in islice(df.itertuples(),10):
 
     ws = [wastes[i] for i, v in enumerate(c[1 : len(wastes) + 1]) if v]
     ss = [storages[i] for i, v in enumerate(c[len(wastes) + 1 :]) if v]
@@ -51,9 +55,93 @@ for c in df.itertuples():
     for w_i, w_p in enumerate(c[1 : len(wastes) + 1]):
         for s_j, s_p in enumerate(c[len(wastes) + 1 :]):
             # print([(wastes[w_i],SELECTORS[w_p]), (storages[s_j],SELECTORS[s_p])])
-            res.loc[(wastes[w_i], SELECTORS[w_p]), (storages[s_j], SELECTORS[s_p])] +=1
+            df.loc[(wastes[w_i], SELECTORS[w_p]), (storages[s_j], SELECTORS[s_p])] += 1
 
-# write in good format
+print(df)
+
+# %%
+
+# write too csv with good format
+# w/ w/o inverted !
 # res.to_csv("data/2_mrcv.csv")
 
 # %%
+
+# marignal sums
+margin_idx = ("*", "Σ")
+margin_cols = df.groupby(level=0).sum().drop_duplicates().reset_index(drop=True)
+margin_cols.index = pd.MultiIndex.from_tuples([margin_idx])
+print(margin_cols)
+
+
+margin_rows = df.groupby(level=0, axis=1).sum().iloc[:, 0]
+margin_rows.name = margin_idx
+margin_rows = pd.DataFrame(margin_rows)
+print(margin_rows)
+
+# margin_rows.loc[('Salt', 'w/o'),margin_idx]
+
+# %%
+# compute expectation from marginal sums :
+# Simultaneous Pairwise Marginal Independence (SPMI)
+
+ex = pd.DataFrame().reindex_like(df)
+
+
+for r_idx in df.index:
+    for c_idx in df.columns:
+        r_margin = margin_rows.loc[r_idx, margin_idx]
+        c_margin = margin_cols.loc[margin_idx, c_idx]
+        # print(r_idx, r_margin, c_idx, c_margin)
+        ex.loc[r_idx, c_idx] = r_margin * c_margin / N
+
+
+# for w_i, w_p in enumerate(c[1 : len(wastes) + 1]):
+#     for s_j, s_p in enumerate(c[len(wastes) + 1 :]):
+#         # print([(wastes[w_i],SELECTORS[w_p]), (storages[s_j],SELECTORS[s_p])])
+#         # df.loc[(wastes[w_i], SELECTORS[w_p]), (storages[s_j], SELECTORS[s_p])] += 1
+#         ex.loc[(wastes[w_i], SELECTORS[w_p]), (storages[s_j], SELECTORS[s_p])] =
+
+
+# %%
+
+
+def odds_ratio(arr):
+    """The trivial one. Just keep the bottom right cell. Amount to compute on "the old matrix" """
+    [FF, FT], [TF, TT] = arr.reshape(2, 2)
+    return TT * FF / TF * FT
+
+
+# redimension the values to a 4D array
+M_2_2 = np.moveaxis(df.values.reshape((I, 2, J, 2)), 1, -2)
+M_4 = M_2_2.reshape((I * J, 4))
+
+print(f"{M_2_2.shape = }")
+print(f"{M_4.shape = }")
+
+
+total = np.sum(M_2_2, axis=(2, 3), keepdims=False)
+print(f"{np.all(total == N) = } (with {N = })")
+
+# %%
+# le cas "Nitrogen"/"Lagoon"
+
+case_observed = df.loc["Nitrogen", "Lagoon"]
+case_expected = ex.loc["Nitrogen", "Lagoon"]
+
+
+print("Nitrogen/Lagoon observed:", odds_ratio(case_observed.values))
+print("Nitrogen/Lagoon expected:", odds_ratio(case_expected.values))
+
+
+# %%
+values = np.moveaxis(df.values.reshape((I, 2, J, 2)), 1, -2).reshape((I * J, 4))
+matrix = np.apply_along_axis(odds_ratio, 1, values).reshape((I, J))
+observed_odds = pd.DataFrame(matrix, index=wastes, columns=storages)
+
+values = np.moveaxis(ex.values.reshape((I, 2, J, 2)), 1, -2).reshape((I * J, 4))
+matrix = np.apply_along_axis(odds_ratio, 1, values).reshape((I, J))
+expected_odds = pd.DataFrame(matrix, index=wastes, columns=storages)
+
+# %%
+(100 * observed_odds / expected_odds).round()
